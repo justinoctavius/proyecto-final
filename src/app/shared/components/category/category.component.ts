c7ad99a5775c9be56d17d896b1e1114334ca7818
justinoctavius <justinoctavio2001@gmail.com>
2022-06-01 22:14:23 -0400

    fix: removing category id

--- src/app/shared/components/category/category.component.ts
+++ src/app/shared/components/category/category.component.ts
@@ -2,19 +2,18 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { faQuestion, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-category-component',
+  selector: 'app-category',
   templateUrl: './category.component.html',
 })
 export class CategoryComponent {
-  @Input() id: string = '';
   @Input() icon: IconDefinition = faQuestion;
   @Input() name: string = 'categoria';
   @Input() color: string = '#7777ff';
   @Input() active: boolean = false;
 
-  @Output() click: EventEmitter<string> = new EventEmitter();
+  @Output() click: EventEmitter<void> = new EventEmitter();
 
   onClick() {
-    this.click.emit(this.id);
+    this.click.emit();
   }
 }
