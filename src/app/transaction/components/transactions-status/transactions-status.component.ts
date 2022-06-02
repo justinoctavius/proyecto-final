e58df69b10c8c90e60c160094979bee16a97673c
justinoctavius <justinoctavio2001@gmail.com>
2022-06-01 21:04:05 -0400

    feat; adding trasactions-status-component

---
+++ src/app/transaction/components/transactions-status/transactions-status.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transactions-status',
+  templateUrl: './transactions-status.component.html',
+})
+export class TransactionsStatusComponent {}
--- src/app/transaction/transaction.module.ts
+++ src/app/transaction/transaction.module.ts
@@ -1,19 +1,24 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { SharedModule } from '../shared/shared.module';
+
 import { TransactionCardComponent } from './components/transaction-card/transaction-card.component';
 import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 import { TransactionsPageComponent } from './pages/transactions-page/transactions-page.component';
+import { TransactionsStatusComponent } from './components/transactions-status/transactions-status.component';
 
 @NgModule({
   declarations: [
     TransactionCardComponent,
     TransactionListComponent,
+    TransactionsStatusComponent,
     TransactionsPageComponent,
   ],
-  imports: [BrowserModule, FontAwesomeModule],
+  imports: [BrowserModule, RouterModule, FontAwesomeModule, SharedModule],
   exports: [TransactionsPageComponent],
 })
 export class TransactionModule {}
