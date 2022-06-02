8511ad7064707e1bb0963bea9eda8d3b3a55e9c2
justinoctavius <justinoctavio2001@gmail.com>
2022-06-01 21:02:00 -0400

    feat: adding IconButton

---
+++ src/app/shared/components/icon-button/icon-button.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { faPlus, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-icon-button',
+  templateUrl: './icon-button.component.html',
+})
+export class IconButtonComponent {
+  @Input() icon: IconDefinition = faPlus;
+  @Input() color: string = 'bg-amber-500';
+  @Output() click: EventEmitter<void> = new EventEmitter();
+
+  handleOnClick() {
+    this.click.emit();
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { IconButtonComponent } from './components/icon-button/icon-button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @NgModule({
-  declarations: [NavbarComponent],
-  imports: [RouterModule],
-  exports: [NavbarComponent],
+  declarations: [NavbarComponent, IconButtonComponent],
+  imports: [RouterModule, FontAwesomeModule],
+  exports: [NavbarComponent, IconButtonComponent],
 })
 export class SharedModule {}
